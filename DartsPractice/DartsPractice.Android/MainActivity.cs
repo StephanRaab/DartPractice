267a5d907481f4a1999a3d0c052d90c373a4b5a1
Stephan Raab <stephan.raab@bd.com>
2021-11-10 15:25:21 -0500

    prepare plugin
    prep plugin to be used

--- DartsPractice/DartsPractice.Android/MainActivity.cs
+++ DartsPractice/DartsPractice.Android/MainActivity.cs
@@ -14,10 +14,17 @@ namespace DartsPractice.Droid
         {
             base.OnCreate(savedInstanceState);
 
+            Rg.Plugins.Popup.Popup.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+        public override void OnBackPressed()
+        {
+            Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
