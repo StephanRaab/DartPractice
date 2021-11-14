1fe2eb955f6f148925b40a7982daa64ffdf09a1c
Stephan Raab <sarcartist@gmail.com>
2021-11-14 00:25:58 -0500

    int to bool for easy conversion

--- DartsPractice/DartsPractice/Models/A1Target.cs
+++ DartsPractice/DartsPractice/Models/A1Target.cs
@@ -6,7 +6,7 @@ namespace DartsPractice.Models
 {
     public class A1Target
     {
-        public ObservableCollection<int> Hits { get; set; }
+        public ObservableCollection<bool> Hits { get; set; }
         public bool IsActive { get; set; }
         public bool IsClosed { get; set; }
     }
